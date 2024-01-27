f9b1c69c712e955cc1905be89d5c41dc2f5959ba
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 17:54:14 +0200

    created on Sat Jan 27 2024 15:54:03 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/dashboard.tsx
@@ -0,0 +1,7 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+    export const Dashboard = () => (
+      <Card>
+      <CardHeader title= "tutoring platform" />
+      <CardContent> A platform for managing students, activities and attendance. < /CardContent>
+      </Card>
+    );
