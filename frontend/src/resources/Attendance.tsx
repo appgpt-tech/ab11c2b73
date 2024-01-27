fd45faf5e6ce9977eb7b87ad0d61b4a19d0f6120
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 17:54:20 +0200

    created on Sat Jan 27 2024 15:54:03 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/Attendance.tsx
@@ -0,0 +1,93 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  //EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  //UrlInput,
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const AttendanceTitle = () => {
+  const record = useRecordContext();
+  return <span>Attendance {record ? `"${ record.id }"` : ""}</span>;
+};
+
+export const AttendanceList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="attendanceId" />
+<TextField source="date" />
+<TextField source="studentId" />
+<TextField source="activityId" />
+<NumberField source="id" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const AttendanceEdit = () => (
+                    <Edit title={<AttendanceTitle />}>
+                      <SimpleForm>
+                          <TextInput source="attendanceId"   />
+<TextInput source="date"   />
+<TextInput source="studentId"   />
+<TextInput source="activityId"   />
+<NumberInput source="id"   disabled/>
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const AttendanceCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="attendanceId"   />
+<TextInput source="date"   />
+<TextInput source="studentId"   />
+<TextInput source="activityId"   />
+<NumberInput source="id"   disabled/>
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+,
+
+    ];
+
+
