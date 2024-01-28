98e412fa6a2155e94a7587613d1a0cf4e2915cf3
11ai <124818466+11ai@users.noreply.github.com>
2024-01-28 12:10:07 +0200

    edited on Sun Jan 28 2024 10:09:36 GMT+0000 (Coordinated Universal Time)

--- backend/src/Database.ts
+++ backend/src/Database.ts
@@ -4,10 +4,10 @@ import { DBConfiguration } from "./Configuration";
 import { SettingsEntity } from "./db/Settings.entity";
 //autogenerate imports based on resources
 import { ProductsEntity } from "./db/Products.entity";
-import { ProductCategoriesEntity } from "./db/ProductCategories.entity";
+import { ProductcategoriesEntity } from "./db/Productcategories.entity";
 import { CustomersEntity } from "./db/Customers.entity";
 import { OrdersEntity } from "./db/Orders.entity";
-import { OrderDetailsEntity } from "./db/OrderDetails.entity";
+import { OrderdetailsEntity } from "./db/Orderdetails.entity";
 import { UsersEntity } from "./db/Users.entity";
 
 export class Database {
@@ -19,7 +19,7 @@ export class Database {
     let dbConfig: any = dbConfiguration as any;
     //Autogenerate entities array from resource names
 
-    dbConfig.entities = [SettingsEntity, ProductsEntity, ProductCategoriesEntity, CustomersEntity, OrdersEntity, OrderDetailsEntity, UsersEntity];
+    dbConfig.entities = [SettingsEntity, ProductsEntity, ProductcategoriesEntity, CustomersEntity, OrdersEntity, OrderdetailsEntity, UsersEntity];
     Database.ds = new DataSource(dbConfig);
     await Database.ds.initialize();
 
@@ -29,7 +29,7 @@ export class Database {
     await Database.Seed();
   }
   static async Seed() {
-    let data: any = {"Products":[{"productId":1,"sku":"SKU123","name":"Product 1","price":50,"weight":1,"description":"This is Product 1.","thumbnail":"UrlToThumb1","image":"UrlToImage1","categoryId":1,"createDate":"2021-09-01","stock":100},{"productId":2,"sku":"SKU234","name":"Product 2","price":75,"weight":1.5,"description":"This is Product 2.","thumbnail":"UrlToThumb2","image":"UrlToImage2","categoryId":2,"createDate":"2021-09-02","stock":200},{"productId":3,"sku":"SKU345","name":"Product 3","price":100,"weight":2,"description":"This is Product 3.","thumbnail":"UrlToThumb3","image":"UrlToImage3","categoryId":3,"createDate":"2021-09-03","stock":300}],"ProductCategories":[{"id":1,"categoryDescription":"Electronics"},{"id":2,"categoryDescription":"Clothing"},{"id":3,"categoryDescription":"Sports"}],"Customers":[{"customerId":1,"email":"user1@example.com","password":"password1","name":"User 1"},{"customerId":2,"email":"user2@example.com","password":"password2","name":"User 2"},{"customerId":3,"email":"user3@example.com","password":"password3","name":"User 3"}],"Orders":[{"orderId":1,"customerId":1,"amount":150,"shippingAddress":"Shipping Address 1","orderAddress":"Order Address 1","orderEmail":"order1@example.com","orderDate":"2021-09-05","orderStatus":"Delivered"},{"orderId":2,"customerId":2,"amount":125,"shippingAddress":"Shipping Address 2","orderAddress":"Order Address 2","orderEmail":"order2@example.com","orderDate":"2021-09-06","orderStatus":"Shipped"},{"orderId":3,"customerId":3,"amount":175,"shippingAddress":"Shipping Address 3","orderAddress":"Order Address 3","orderEmail":"order3@example.com","orderDate":"2021-09-07","orderStatus":"Pending"}],"OrderDetails":[{"id":1,"orderId":1,"productId":1,"price":50,"sku":"SKU123","quantity":3},{"id":2,"orderId":2,"productId":2,"price":75,"sku":"SKU234","quantity":2},{"id":3,"orderId":3,"productId":3,"price":100,"sku":"SKU345","quantity":2}],"Users":[{"id":1,"name":"User 1","email":"admin1@example.com","username":"admin1","password":"admin123","role":"Admin","dateCreated":"2021-09-01"},{"id":2,"name":"User 2","email":"admin2@example.com","username":"admin2","password":"admin234","role":"Admin","dateCreated":"2021-09-02"},{"id":3,"name":"User 3","email":"admin3@example.com","username":"admin3","password":"admin345","role":"Admin","dateCreated":"2021-09-03"}]};
+    let data: any = {"Products":[{"productId":1,"sku":"PROD1","name":"Product 1","price":100,"weight":50,"description":"This is product 1","thumbnail":"url","image":"url","categoryId":1,"createDate":"2022-01-01","stock":10},{"productId":2,"sku":"PROD2","name":"Product 2","price":200,"weight":70,"description":"This is product 2","thumbnail":"url","image":"url","categoryId":2,"createDate":"2022-01-01","stock":15},{"productId":3,"sku":"PROD3","name":"Product 3","price":150,"weight":60,"description":"This is product 3","thumbnail":"url","image":"url","categoryId":1,"createDate":"2022-01-01","stock":20}],"Productcategories":[{"id":1,"categoryDescription":"Category 1"},{"id":2,"categoryDescription":"Category 2"},{"id":3,"categoryDescription":"Category 3"}],"Customers":[{"customerId":1,"email":"cust1@email.com","password":"password1","name":"Customer 1","lastOrderDate":"2022-01-01"},{"customerId":2,"email":"cust2@email.com","password":"password2","name":"Customer 2","lastOrderDate":"2022-01-02"},{"customerId":3,"email":"cust3@email.com","password":"password3","name":"Customer 3","lastOrderDate":"2022-01-03"}],"Orders":[{"orderId":1,"customerId":1,"amount":100,"shippingAddress":"Address 1","orderAddress":"Address 1","orderEmail":"cust1@email.com","orderDate":"2022-01-01","orderStatus":"Delivered"},{"orderId":2,"customerId":2,"amount":200,"shippingAddress":"Address 2","orderAddress":"Address 2","orderEmail":"cust2@email.com","orderDate":"2022-01-02","orderStatus":"Shipped"},{"orderId":3,"customerId":3,"amount":150,"shippingAddress":"Address 3","orderAddress":"Address 3","orderEmail":"cust3@email.com","orderDate":"2022-01-03","orderStatus":"Processing"}],"Orderdetails":[{"id":1,"orderId":1,"productId":1,"price":100,"sku":"PROD1","quantity":1},{"id":2,"orderId":2,"productId":2,"price":200,"sku":"PROD2","quantity":1},{"id":3,"orderId":3,"productId":3,"price":150,"sku":"PROD3","quantity":1}],"Users":[{"id":1,"name":"User 1","email":"user1@email.com","username":"user1","password":"password1","role":"Admin","dateCreated":"2022-01-01"},{"id":2,"name":"User 2","email":"user2@email.com","username":"user2","password":"password2","role":"User","dateCreated":"2022-01-02"},{"id":3,"name":"User 3","email":"user3@email.com","username":"user3","password":"password3","role":"User","dateCreated":"2022-01-03"}]};
     //Autogenerate multiple such calls ie for each resource and its data object
     let isSeeded = await this.IsSeeded();
     //if (!isSeeded) {
@@ -37,10 +37,10 @@ export class Database {
     if (true){
       console.log('   Seeding database...');
       await this.SeedResource("ProductsEntity", data.Products);
-await this.SeedResource("ProductCategoriesEntity", data.ProductCategories);
+await this.SeedResource("ProductcategoriesEntity", data.Productcategories);
 await this.SeedResource("CustomersEntity", data.Customers);
 await this.SeedResource("OrdersEntity", data.Orders);
-await this.SeedResource("OrderDetailsEntity", data.OrderDetails);
+await this.SeedResource("OrderdetailsEntity", data.Orderdetails);
 await this.SeedResource("UsersEntity", data.Users); 
       await this.SeedResource("SettingsEntity", {
         settingname: "isSeeded",
