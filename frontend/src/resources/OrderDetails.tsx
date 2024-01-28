65316b581c2e04ddac373e36d08a82dc269d53bc
11ai <124818466+11ai@users.noreply.github.com>
2024-01-28 12:04:57 +0200

    created on Sun Jan 28 2024 10:04:42 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/OrderDetails.tsx
@@ -0,0 +1,97 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  //EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  //UrlInput,
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const OrderDetailsTitle = () => {
+  const record = useRecordContext();
+  return <span>OrderDetails {record ? `"${ record.id }"` : ""}</span>;
+};
+
+export const OrderDetailsList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="id" />
+<TextField source="orderId" />
+<TextField source="productId" />
+<TextField source="price" />
+<TextField source="sku" />
+<TextField source="quantity" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const OrderDetailsEdit = () => (
+                    <Edit title={<OrderDetailsTitle />}>
+                      <SimpleForm>
+                          <TextInput source="id"   disabled/>
+<TextInput source="orderId"   />
+<TextInput source="productId"   />
+<TextInput source="price"   />
+<TextInput source="sku"   />
+<TextInput source="quantity"   />
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const OrderDetailsCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="id"   disabled/>
+<TextInput source="orderId"   />
+<TextInput source="productId"   />
+<TextInput source="price"   />
+<TextInput source="sku"   />
+<TextInput source="quantity"   />
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+,
+,
+
+    ];
+
+
