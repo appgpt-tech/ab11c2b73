0a812ffb5ee547e1f3eff0c17a30fae552f7364e
11ai <124818466+11ai@users.noreply.github.com>
2024-01-28 12:04:50 +0200

    edited on Sun Jan 28 2024 10:04:42 GMT+0000 (Coordinated Universal Time)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,12 +8,18 @@ import { authProvider, apInitialize } from "./authProvider";
 import { i18nProvider } from "./i18nProvider";
 import LoginPage, { Login } from "./Login";
 import data from "./data";
-import { StudentList, StudentCreate, StudentEdit} from "./resources/Student";
-import { ActivityList, ActivityCreate, ActivityEdit} from "./resources/Activity";
-import { AttendanceList, AttendanceCreate, AttendanceEdit} from "./resources/Attendance";
-import StudentIcon from "@mui/icons-material/Person";
-import ActivityIcon from "@mui/icons-material/FitnessCenter";
-import AttendanceIcon from "@mui/icons-material/CalendarToday"; 
+import { ProductsList, ProductsCreate, ProductsEdit} from "./resources/Products";
+import { ProductCategoriesList, ProductCategoriesCreate, ProductCategoriesEdit} from "./resources/ProductCategories";
+import { CustomersList, CustomersCreate, CustomersEdit} from "./resources/Customers";
+import { OrdersList, OrdersCreate, OrdersEdit} from "./resources/Orders";
+import { OrderDetailsList, OrderDetailsCreate, OrderDetailsEdit} from "./resources/OrderDetails";
+import { UsersList, UsersCreate, UsersEdit} from "./resources/Users";
+import ProductsIcon from "@mui/icons-material/LocalMall";
+import ProductCategoriesIcon from "@mui/icons-material/Category";
+import CustomersIcon from "@mui/icons-material/People";
+import OrdersIcon from "@mui/icons-material/ShoppingCart";
+import OrderDetailsIcon from "@mui/icons-material/Details";
+import UsersIcon from "@mui/icons-material/PeopleAlt"; 
 // SUPERTOKENS
 import React from "react";
 import SuperTokens, {
@@ -75,24 +81,42 @@ const App = () => (
         dashboard={Dashboard}
         
       >
-    <Resource name="Student" options={{label:"student"}} 
-list={StudentList}
-create={StudentCreate}
-edit={StudentEdit}
+    <Resource name="Products" options={{label:"products"}} 
+list={ProductsList}
+create={ProductsCreate}
+edit={ProductsEdit}
+recordRepresentation="name"
+icon={ProductsIcon}/>
+<Resource name="ProductCategories" options={{label:"productCategories"}} 
+list={ProductCategoriesList}
+create={ProductCategoriesCreate}
+edit={ProductCategoriesEdit}
 recordRepresentation="id"
-icon={StudentIcon}/>
-<Resource name="Activity" options={{label:"activity"}} 
-list={ActivityList}
-create={ActivityCreate}
-edit={ActivityEdit}
+icon={ProductCategoriesIcon}/>
+<Resource name="Customers" options={{label:"customers"}} 
+list={CustomersList}
+create={CustomersCreate}
+edit={CustomersEdit}
+recordRepresentation="name"
+icon={CustomersIcon}/>
+<Resource name="Orders" options={{label:"orders"}} 
+list={OrdersList}
+create={OrdersCreate}
+edit={OrdersEdit}
 recordRepresentation="id"
-icon={ActivityIcon}/>
-<Resource name="Attendance" options={{label:"attendance"}} 
-list={AttendanceList}
-create={AttendanceCreate}
-edit={AttendanceEdit}
+icon={OrdersIcon}/>
+<Resource name="OrderDetails" options={{label:"orderDetails"}} 
+list={OrderDetailsList}
+create={OrderDetailsCreate}
+edit={OrderDetailsEdit}
 recordRepresentation="id"
-icon={AttendanceIcon}/>
+icon={OrderDetailsIcon}/>
+<Resource name="Users" options={{label:"users"}} 
+list={UsersList}
+create={UsersCreate}
+edit={UsersEdit}
+recordRepresentation="id"
+icon={UsersIcon}/>
     <CustomRoutes noLayout>
       {/*This renders the login UI on the /auth route*/}
       {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
