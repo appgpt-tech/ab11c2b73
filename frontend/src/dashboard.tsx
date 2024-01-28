75fdbae3c7252ed6f0d9b432a898b21961887fb3
11ai <124818466+11ai@users.noreply.github.com>
2024-01-28 10:44:03 +0200

    edited on Sun Jan 28 2024 08:43:49 GMT+0000 (Coordinated Universal Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
-      <CardHeader title= "student activity management" />
-      <CardContent> An app to manage student activities and attendance records in an academic institution. < /CardContent>
+      <CardHeader title= "tutoring management system" />
+      <CardContent> An application for managing tutoring lessons, activities, students, their attendance and costs. < /CardContent>
       </Card>
     );
