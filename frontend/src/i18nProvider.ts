c86e7fef206d93957e5ed1a21adde72b60b7cb2a
11ai <124818466+11ai@users.noreply.github.com>
2024-01-28 12:25:19 +0200

    edited on Sun Jan 28 2024 10:25:06 GMT+0000 (Coordinated Universal Time)

--- frontend/src/i18nProvider.ts
+++ frontend/src/i18nProvider.ts
@@ -3,7 +3,7 @@
     import polyglotI18nProvider from "ra-i18n-polyglot";
     import enOriginal from 'ra-language-english';
 
-    const enResources = { resources: {"Products":{"name":"products","fields":{"productId":"productId","sku":"sku","name":"name","price":"price","weight":"weight","description":"description","thumbnail":"thumbnail","image":"image","categoryId":"categoryId","createDate":"createDate","stock":"stock","id":"id"}},"Productcategories":{"name":"productCategories","fields":{"id":"id","categoryDescription":"categoryDescription"}}}};
+    const enResources = { resources: {"Products":{"name":"products","fields":{"productId":"productId","sku":"sku","name":"name","price":"price","weight":"weight","description":"description","thumbnail":"thumbnail","image":"image","categoryId":"categoryId","createDate":"createDate","stock":"stock","id":"id"}},"Productcategories":{"name":"productCategories","fields":{"id":"id","categoryDescription":"categoryDescription"}},"Customers":{"name":"customers","fields":{"customerId":"customerId","email":"email","password":"password","name":"name","id":"id"}},"Orders":{"name":"orders","fields":{"orderId":"orderId","customerId":"customerId","amount":"amount","shippingAddress":"shippingAddress","orderAddress":"orderAddress","orderEmail":"orderEmail","orderDate":"orderDate","orderStatus":"orderStatus","id":"id"}},"Orderdetails":{"name":"orderDetails","fields":{"id":"id","orderId":"orderId","productId":"productId","price":"price","sku":"sku","quantity":"quantity"}},"Users":{"name":"users","fields":{"id":"id","name":"name","email":"email","username":"username","password":"password","role":"role","dateCreated":"dateCreated"}}}};
 
 
     const en = mergeTranslations(enOriginal,enResources);
