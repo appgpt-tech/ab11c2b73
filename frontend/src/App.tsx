1b304d3e613232f70cf2928c8c45142ed891c94d
11ai <124818466+11ai@users.noreply.github.com>
2024-01-28 12:13:07 +0200

    edited on Sun Jan 28 2024 10:12:56 GMT+0000 (Coordinated Universal Time)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,16 +10,8 @@ import LoginPage, { Login } from "./Login";
 import data from "./data";
 import { ProductsList, ProductsCreate, ProductsEdit} from "./resources/Products";
 import { ProductcategoriesList, ProductcategoriesCreate, ProductcategoriesEdit} from "./resources/Productcategories";
-import { CustomersList, CustomersCreate, CustomersEdit} from "./resources/Customers";
-import { OrdersList, OrdersCreate, OrdersEdit} from "./resources/Orders";
-import { OrderdetailsList, OrderdetailsCreate, OrderdetailsEdit} from "./resources/Orderdetails";
-import { UsersList, UsersCreate, UsersEdit} from "./resources/Users";
 import ProductsIcon from "@mui/icons-material/ProductionQuantityLimits";
-import ProductcategoriesIcon from "@mui/icons-material/Category";
-import CustomersIcon from "@mui/icons-material/People";
-import OrdersIcon from "@mui/icons-material/Reorder";
-import OrderdetailsIcon from "@mui/icons-material/Details";
-import UsersIcon from "@mui/icons-material/Person"; 
+import ProductcategoriesIcon from "@mui/icons-material/Category"; 
 // SUPERTOKENS
 import React from "react";
 import SuperTokens, {
@@ -93,30 +85,6 @@ create={ProductcategoriesCreate}
 edit={ProductcategoriesEdit}
 recordRepresentation="id"
 icon={ProductcategoriesIcon}/>
-<Resource name="Customers" options={{label:"customers"}} 
-list={CustomersList}
-create={CustomersCreate}
-edit={CustomersEdit}
-recordRepresentation="name"
-icon={CustomersIcon}/>
-<Resource name="Orders" options={{label:"orders"}} 
-list={OrdersList}
-create={OrdersCreate}
-edit={OrdersEdit}
-recordRepresentation="id"
-icon={OrdersIcon}/>
-<Resource name="Orderdetails" options={{label:"orderDetails"}} 
-list={OrderdetailsList}
-create={OrderdetailsCreate}
-edit={OrderdetailsEdit}
-recordRepresentation="id"
-icon={OrderdetailsIcon}/>
-<Resource name="Users" options={{label:"users"}} 
-list={UsersList}
-create={UsersCreate}
-edit={UsersEdit}
-recordRepresentation="id"
-icon={UsersIcon}/>
     <CustomRoutes noLayout>
       {/*This renders the login UI on the /auth route*/}
       {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
