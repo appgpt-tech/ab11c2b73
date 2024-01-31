1f840420266b36c6818ade155cf87f6c5e627ebb
11ai <124818466+11ai@users.noreply.github.com>
2024-01-31 20:46:21 +0200

    created on Wed Jan 31 2024 20:46:20 GMT+0200 (Eastern European Standard Time)

---
+++ frontend\src\dashboard.tsx
@@ -0,0 +1,7 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+    export const Dashboard = () => (
+      <Card>
+      <CardHeader title= "null" />
+      <CardContent> Built by appGPT < /CardContent>
+      </Card>
+    );
