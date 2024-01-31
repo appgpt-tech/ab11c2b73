9d217c4a2dbdd4e1d25d996e3adb10b224f98797
11ai <124818466+11ai@users.noreply.github.com>
2024-01-31 21:06:31 +0200

    edited on Wed Jan 31 2024 19:06:19 GMT+0000 (Coordinated Universal Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
-      <CardHeader title= "ecommerceapp" />
-      <CardContent> An application managing an eCommerce store < /CardContent>
+      <CardHeader title= "BookReviewApp" />
+      <CardContent> An application for reviewing books < /CardContent>
       </Card>
     );
