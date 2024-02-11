f6d3af2945a8c410f34accbef57e13355f062054
11ai <124818466+11ai@users.noreply.github.com>
2024-02-11 10:02:10 +0200

    created on Sun Feb 11 2024 08:01:53 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/discounts.tsx
@@ -0,0 +1,104 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  UrlInput,
+  PasswordInput
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ReadOnlyPasswordField = ({ record, source }) => {
+
+  // You can customize the way you display the password here, e.g., mask it with asterisks
+  const maskedPassword =  '********';
+
+  return (
+      <span>{maskedPassword}</span>
+  );
+};
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const discountsTitle = () => {
+  const record = useRecordContext();
+  return <span>discounts {record ? `"${ record.discountName }"` : ""}</span>;
+};
+
+export const discountsList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <NumberField source="product_id" />
+<TextField source="discountName" />
+
+<NumberField source="discountAmount" />
+<NumberField source="discountPercent" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const discountsEdit = () => (
+                    <Edit title={<discountsTitle />}>
+                      <SimpleForm>
+                          <NumberInput source="product_id"   />
+<TextInput source="discountName"   />
+<TextInput source="description"   />
+<NumberInput source="discountAmount"   />
+<NumberInput source="discountPercent"   />
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const discountsCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <NumberInput source="product_id"   />
+<TextInput source="discountName"   />
+<TextInput source="description"   />
+<NumberInput source="discountAmount"   />
+<NumberInput source="discountPercent"   />
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+,
+,
+
+    ];
+
+
