1997ee0dfad0605c2a1d93001fe79d12f2059978
11ai <124818466+11ai@users.noreply.github.com>
2024-02-11 10:08:07 +0200

    created on Sun Feb 11 2024 08:07:39 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/SupportTickets.tsx
@@ -0,0 +1,108 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  UrlInput,
+  PasswordInput
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ReadOnlyPasswordField = ({ record, source }) => {
+
+  // You can customize the way you display the password here, e.g., mask it with asterisks
+  const maskedPassword =  '********';
+
+  return (
+      <span>{maskedPassword}</span>
+  );
+};
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const SupportTicketsTitle = () => {
+  const record = useRecordContext();
+  return <span>SupportTickets {record ? `"${ record.userId }"` : ""}</span>;
+};
+
+export const SupportTicketsList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <ReferenceField source="userId" reference="Users"  />
+<ReferenceField source="customerId" reference="Customers"  />
+
+<TextField source="status" />
+<DateField source="creationDate" />
+<DateField source="lastUpdateDate" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const SupportTicketsEdit = () => (
+                    <Edit title={<SupportTicketsTitle />}>
+                      <SimpleForm>
+                          <ReferenceInput source="userId"  reference="Users"   />
+<ReferenceInput source="customerId"  reference="Customers"   />
+<TextInput source="description"   />
+<TextInput source="status"   />
+<DateInput source="creationDate"   />
+<DateInput source="lastUpdateDate"   />
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const SupportTicketsCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <ReferenceInput source="userId"  reference="Users"   />
+<ReferenceInput source="customerId"  reference="Customers"   />
+<TextInput source="description"   />
+<TextInput source="status"   />
+<DateInput source="creationDate"   />
+<DateInput source="lastUpdateDate"   />
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+<ReferenceInput source="userId" label="userId" reference="Users"   alwaysOn/>,
+<ReferenceInput source="customerId" label="customerId" reference="Customers"   alwaysOn/>,
+,
+,
+,
+
+    ];
+
+
