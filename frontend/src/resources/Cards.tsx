aca2d2aad63697d3208d3e0f0e8680115f221406
11ai <124818466+11ai@users.noreply.github.com>
2024-02-11 15:12:33 +0200

    created on Sun Feb 11 2024 13:12:16 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/Cards.tsx
@@ -0,0 +1,116 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  UrlInput,
+  PasswordInput
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ReadOnlyPasswordField = ({ record, source }) => {
+
+  // You can customize the way you display the password here, e.g., mask it with asterisks
+  const maskedPassword =  '********';
+
+  return (
+      <span>{maskedPassword}</span>
+  );
+};
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const CardsTitle = () => {
+  const record = useRecordContext();
+  return <span>Cards {record ? `"${ record.cardId }"` : ""}</span>;
+};
+
+export const CardsList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <NumberField source="cardId" />
+<ReferenceField source="setId" reference="Sets"  />
+<TextField source="cardName" />
+<TextField source="serial" />
+<TextField source="type" />
+<TextField source="rarity" />
+<TextField source="condition" />
+<ImageField source="imageUrl" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const CardsEdit = () => (
+                    <Edit title={<CardsTitle />}>
+                      <SimpleForm>
+                          <NumberInput source="cardId"   />
+<ReferenceInput source="setId"  reference="Sets"   />
+<TextInput source="cardName"   />
+<TextInput source="serial"   />
+<TextInput source="type"   />
+<TextInput source="rarity"   />
+<TextInput source="condition"   />
+<ImageInput source="imageUrl"   />
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const CardsCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <NumberInput source="cardId"   />
+<ReferenceInput source="setId"  reference="Sets"   />
+<TextInput source="cardName"   />
+<TextInput source="serial"   />
+<TextInput source="type"   />
+<TextInput source="rarity"   />
+<TextInput source="condition"   />
+<ImageInput source="imageUrl"   />
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+<ReferenceInput source="setId" label="setId" reference="Sets"   alwaysOn/>,
+,
+,
+,
+,
+,
+,
+
+    ];
+
+
