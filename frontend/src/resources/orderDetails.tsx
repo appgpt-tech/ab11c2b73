cab9fd481257c4d7c1e7f3ca6873045023116884
11ai <124818466+11ai@users.noreply.github.com>
2024-02-11 10:02:15 +0200

    created on Sun Feb 11 2024 08:01:53 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/orderDetails.tsx
@@ -0,0 +1,100 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  UrlInput,
+  PasswordInput
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ReadOnlyPasswordField = ({ record, source }) => {
+
+  // You can customize the way you display the password here, e.g., mask it with asterisks
+  const maskedPassword =  '********';
+
+  return (
+      <span>{maskedPassword}</span>
+  );
+};
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const orderDetailsTitle = () => {
+  const record = useRecordContext();
+  return <span>orderDetails {record ? `"${ record.id }"` : ""}</span>;
+};
+
+export const orderDetailsList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <NumberField source="orderId" />
+<NumberField source="productId" />
+<NumberField source="price" />
+<NumberField source="quantity" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const orderDetailsEdit = () => (
+                    <Edit title={<orderDetailsTitle />}>
+                      <SimpleForm>
+                          <NumberInput source="orderId"   />
+<NumberInput source="productId"   />
+<NumberInput source="price"   />
+<NumberInput source="quantity"   />
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const orderDetailsCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <NumberInput source="orderId"   />
+<NumberInput source="productId"   />
+<NumberInput source="price"   />
+<NumberInput source="quantity"   />
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+,
+
+    ];
+
+
