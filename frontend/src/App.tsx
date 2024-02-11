75e4cb0042d8c4451667cb211c27f13786dfbbb1
11ai <124818466+11ai@users.noreply.github.com>
2024-02-11 10:02:02 +0200

    edited on Sun Feb 11 2024 08:01:52 GMT+0000 (Coordinated Universal Time)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,39 @@
 
 // in src/App.tsx
 import { Admin, Resource, CustomRoutes } from "react-admin";
-import postgrestRestProvider from "@promitheus/ra-data-postgrest";
+import { customDataProvider } from "./dataProvider";
 import fakeDataProvider from "ra-data-fakerest";
 import { Dashboard } from "./dashboard";
 import { authProvider, apInitialize } from "./authProvider";
 import { i18nProvider } from "./i18nProvider";
 import LoginPage, { Login } from "./Login";
 import data from "./data";
-import { BooksList, BooksCreate, BooksEdit} from "./resources/Books";
-import { AuthorsList, AuthorsCreate, AuthorsEdit} from "./resources/Authors";
-import { GenresList, GenresCreate, GenresEdit} from "./resources/Genres";
-import BooksIcon from "@mui/icons-material/Book";
-import AuthorsIcon from "@mui/icons-material/Person";
-import GenresIcon from "@mui/icons-material/Category"; 
+import { usersList, usersCreate, usersEdit} from "./resources/users";
+import { vendorsList, vendorsCreate, vendorsEdit} from "./resources/vendors";
+import { customersList, customersCreate, customersEdit} from "./resources/customers";
+import { inventoryList, inventoryCreate, inventoryEdit} from "./resources/inventory";
+import { productsList, productsCreate, productsEdit} from "./resources/products";
+import { productCategoriesList, productCategoriesCreate, productCategoriesEdit} from "./resources/productCategories";
+import { discountsList, discountsCreate, discountsEdit} from "./resources/discounts";
+import { shoppingCartsList, shoppingCartsCreate, shoppingCartsEdit} from "./resources/shoppingCarts";
+import { ordersList, ordersCreate, ordersEdit} from "./resources/orders";
+import { orderDetailsList, orderDetailsCreate, orderDetailsEdit} from "./resources/orderDetails";
+import { paymentsList, paymentsCreate, paymentsEdit} from "./resources/payments";
+import { reviewsList, reviewsCreate, reviewsEdit} from "./resources/reviews";
+import { supportTicketsList, supportTicketsCreate, supportTicketsEdit} from "./resources/supportTickets";
+import usersIcon from "@mui/icons-material/SupervisedUserCircle";
+import vendorsIcon from "@mui/icons-material/Store";
+import customersIcon from "@mui/icons-material/People";
+import inventoryIcon from "@mui/icons-material/Inventory";
+import productsIcon from "@mui/icons-material/Category";
+import productCategoriesIcon from "@mui/icons-material/Category";
+import discountsIcon from "@mui/icons-material/LocalOffer";
+import shoppingCartsIcon from "@mui/icons-material/ShoppingCart";
+import ordersIcon from "@mui/icons-material/Receipt";
+import orderDetailsIcon from "@mui/icons-material/Assignment";
+import paymentsIcon from "@mui/icons-material/Payment";
+import reviewsIcon from "@mui/icons-material/RateReview";
+import supportTicketsIcon from "@mui/icons-material/Support"; 
 // SUPERTOKENS
 import React from "react";
 import SuperTokens, {
@@ -52,7 +72,7 @@ apInitialize(Session);
 // END SUPERTOKENS
 let dataProvider: any;
 if (import.meta.env.VITE_USE_BACKEND_DATA === "true") {
-  dataProvider = postgrestRestProvider(
+  dataProvider = customDataProvider(
     import.meta.env.VITE_BACKEND_DOMAIN +
       import.meta.env.VITE_BACKEND_APIPATH +
       "/proxy"
@@ -75,24 +95,84 @@ const App = () => (
         dashboard={Dashboard}
         
       >
-    <Resource name="Books" options={{label:"books"}} 
-list={BooksList}
-create={BooksCreate}
-edit={BooksEdit}
-recordRepresentation="Title"
-icon={BooksIcon}/>
-<Resource name="Authors" options={{label:"authors"}} 
-list={AuthorsList}
-create={AuthorsCreate}
-edit={AuthorsEdit}
-recordRepresentation="Name"
-icon={AuthorsIcon}/>
-<Resource name="Genres" options={{label:"genres"}} 
-list={GenresList}
-create={GenresCreate}
-edit={GenresEdit}
-recordRepresentation="Category"
-icon={GenresIcon}/>
+    <Resource name="users" options={{label:"Users"}} 
+list={usersList}
+create={usersCreate}
+edit={usersEdit}
+recordRepresentation="name"
+icon={usersIcon}/>
+<Resource name="vendors" options={{label:"Vendors"}} 
+list={vendorsList}
+create={vendorsCreate}
+edit={vendorsEdit}
+recordRepresentation="companyName"
+icon={vendorsIcon}/>
+<Resource name="customers" options={{label:"Customers"}} 
+list={customersList}
+create={customersCreate}
+edit={customersEdit}
+recordRepresentation="email"
+icon={customersIcon}/>
+<Resource name="inventory" options={{label:"Inventory"}} 
+list={inventoryList}
+create={inventoryCreate}
+edit={inventoryEdit}
+recordRepresentation="productId"
+icon={inventoryIcon}/>
+<Resource name="products" options={{label:"Products"}} 
+list={productsList}
+create={productsCreate}
+edit={productsEdit}
+recordRepresentation="name"
+icon={productsIcon}/>
+<Resource name="productCategories" options={{label:"Product Categories"}} 
+list={productCategoriesList}
+create={productCategoriesCreate}
+edit={productCategoriesEdit}
+recordRepresentation="id"
+icon={productCategoriesIcon}/>
+<Resource name="discounts" options={{label:"Discounts"}} 
+list={discountsList}
+create={discountsCreate}
+edit={discountsEdit}
+recordRepresentation="discountName"
+icon={discountsIcon}/>
+<Resource name="shoppingCarts" options={{label:"Shopping Carts"}} 
+list={shoppingCartsList}
+create={shoppingCartsCreate}
+edit={shoppingCartsEdit}
+recordRepresentation="customerId"
+icon={shoppingCartsIcon}/>
+<Resource name="orders" options={{label:"Orders"}} 
+list={ordersList}
+create={ordersCreate}
+edit={ordersEdit}
+recordRepresentation="id"
+icon={ordersIcon}/>
+<Resource name="orderDetails" options={{label:"Order Details"}} 
+list={orderDetailsList}
+create={orderDetailsCreate}
+edit={orderDetailsEdit}
+recordRepresentation="id"
+icon={orderDetailsIcon}/>
+<Resource name="payments" options={{label:"Payments"}} 
+list={paymentsList}
+create={paymentsCreate}
+edit={paymentsEdit}
+recordRepresentation="orderId"
+icon={paymentsIcon}/>
+<Resource name="reviews" options={{label:"Reviews"}} 
+list={reviewsList}
+create={reviewsCreate}
+edit={reviewsEdit}
+recordRepresentation="productId"
+icon={reviewsIcon}/>
+<Resource name="supportTickets" options={{label:"Support Tickets"}} 
+list={supportTicketsList}
+create={supportTicketsCreate}
+edit={supportTicketsEdit}
+recordRepresentation="id"
+icon={supportTicketsIcon}/>
     <CustomRoutes noLayout>
       {/*This renders the login UI on the /auth route*/}
       {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
